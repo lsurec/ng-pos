f55f8884461bf6ff7fd71c4c6c8a550c94b7e5ce
Robert Morales <lsurec@demosoft.com.gt>
2023-12-20 15:43:45 -0600

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ApiComponent } from './components/api/api.component';
+import { LoginComponent } from './components/login/login.component';
+import { SplashComponent } from './components/splash/splash.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { HelpComponent } from './components/help/help.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiComponent,
+    LoginComponent,
+    SplashComponent,
+    NotFoundComponent,
+    HomeComponent,
+    HelpComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule
