79752c94e7723e8d60f758f62958758fb4d7833e
Robert Morales <lsurec@demosoft.com.gt>
2024-01-03 12:34:30 -0600

    .

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -97,7 +97,6 @@ export class LoginComponent {
     PreferencesService.user = resLogin.user;
     PreferencesService.conStr = this._encryptService.encrypt(resLogin.con)
 
-    
 
     let user = PreferencesService.user;
     let token = PreferencesService.token;
