f0e1bd4a19c6d45a1481cc39c28a1e66648af347
Robert Morales <lsurec@demosoft.com.gt>
2024-01-03 08:57:43 -0600

    add keys in storage service

--- src/app/services/preferences.service.ts
+++ src/app/services/preferences.service.ts
@@ -3,81 +3,129 @@ import { Injectable, Input } from "@angular/core";
 @Injectable()
 export class PreferencesService {
 
+    
+    private static userKey = 'user';
+    private static userStorageKey = 'userStorage';
+    private static tokenKey = 'token';
+    private static tokenStorageKey = 'tokenStorage';
+    private static languageKey = 'language';
+    private static empresaKey = 'empresa';
+    private static estacionKey = 'estacion';
+    private static temaKey = 'tema';
+    private static urlKey = 'url';
+    private static con = 'conStr';
+
     @Input()
     static set lang(value: string) {
-        localStorage.setItem("language", value);
+        localStorage.setItem(this.languageKey, value);
     }
 
     static get lang(): string {
-        let language = localStorage.getItem("language");
-        if (!language) return "";
-        return language;
+        let value = localStorage.getItem(this.languageKey);
+        if (!value) return "";
+        return value;
+    }
+
+    @Input()
+    static set tokenStorage(value: string) {
+        localStorage.setItem(this.tokenStorageKey, value);
+    }
+
+    static get tokenStorage(): string {
+        let value = localStorage.getItem(this.tokenStorageKey);
+        if (!value) return "";
+        return value;
     }
 
     @Input()
     static set token(value: string) {
-        localStorage.setItem("token", value);
+        localStorage.setItem(this.tokenKey, value);
     }
 
     static get token(): string {
-        let language = localStorage.getItem("token");
-        if (!language) return "";
-        return language;
+        let value = localStorage.getItem(this.tokenKey);
+        if (!value) return "";
+        return value;
     }
+
+    @Input()
+    static set userStorage(value: string) {
+        localStorage.setItem(this.userStorageKey, value);
+    }
+
+    static get userStorage(): string {
+        let value = localStorage.getItem(this.userStorageKey);
+        if (!value) return "";
+        return value;
+    }
+
+
     @Input()
     static set user(value: string) {
-        localStorage.setItem("user", value);
+        localStorage.setItem(this.userKey, value);
     }
 
     static get user(): string {
-        let language = localStorage.getItem("user");
-        if (!language) return "";
-        return language;
+        let value = localStorage.getItem(this.userKey);
+        if (!value) return "";
+        return value;
     }
 
 
     @Input()
     static set empresa(value: string) {
-        sessionStorage.setItem("empresa", value);
+        sessionStorage.setItem(this.empresaKey, value);
     }
 
     static get empresa(): string {
-        let empresaSeleccionada = sessionStorage.getItem("empresa");
-        if (!empresaSeleccionada) return "";
-        return empresaSeleccionada;
+        let value = sessionStorage.getItem(this.empresaKey);
+        if (!value) return "";
+        return value;
     }
 
     @Input()
     static set estacion(value: string) {
-        sessionStorage.setItem("estacion", value);
+        sessionStorage.setItem(this.estacionKey, value);
     }
 
     static get estacion(): string {
-        let estacionSeleccionada = sessionStorage.getItem("estacion");
-        if (!estacionSeleccionada) return "";
-        return estacionSeleccionada;
+        let value = sessionStorage.getItem(this.estacionKey);
+        if (!value) return "";
+        return value;
     }
 
     @Input()
     static set theme(value: string) {
-        localStorage.setItem("tema", value);
+        localStorage.setItem(this.temaKey, value);
     }
 
     static get theme(): string {
-        let tema = localStorage.getItem("tema");
-        if (!tema) return "";
-        return tema;
+        let value = localStorage.getItem(this.temaKey);
+        if (!value) return "";
+        return value;
     }
 
     @Input()
     static set baseUrl(value: string) {
-        localStorage.setItem("baseUrl", value);
+        localStorage.setItem(this.urlKey, value);
     }
 
     static get baseUrl(): string {
-        let baseUrl = localStorage.getItem("baseUrl");
-        if (!baseUrl) return "";
-        return baseUrl;
+        let value = localStorage.getItem(this.urlKey);
+        if (!value) return "";
+        return value;
+    }
+
+
+    @Input()
+    static set conStr(value: string) {
+        localStorage.setItem(this.conStr, value);
+    }
+
+    static get conStr(): string {
+        let value = localStorage.getItem(this.conStr);
+        if (!value) return "";
+        return value;
     }
 
 }
