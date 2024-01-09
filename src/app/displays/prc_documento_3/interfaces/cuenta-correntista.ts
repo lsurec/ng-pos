542116b34111cf5e3b5b5f787aee7f365fcec127
Robert Morales <lsurec@demosoft.com.gt>
2024-01-09 15:02:07 -0600

    add cuneta service

---
+++ src/app/displays/prc_documento_3/interfaces/cuenta-correntista.ts
@@ -0,0 +1,8 @@
+export interface CuentaCorrentistaInterface {
+    id:        number;
+    nombre:    string;
+    direccion: string;
+    telefono:  string;
+    correo:    string;
+    nit:       string;
+}
