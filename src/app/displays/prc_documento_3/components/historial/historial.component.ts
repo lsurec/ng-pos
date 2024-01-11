d16303e7e9c813abdffada4cb41f058480b9a66a
Catu <karlacatusurec@gmail.com>
2024-01-11 11:00:24 -0600

    resumen de docuentos listo

--- src/app/displays/prc_documento_3/components/historial/historial.component.ts
+++ src/app/displays/prc_documento_3/components/historial/historial.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EventService } from 'src/app/services/event.service';
+import { DocumentoResumenInterface } from '../../interfaces/documento-resumen.interface';
 
 @Component({
   selector: 'app-historial',
@@ -9,6 +10,35 @@ import { EventService } from 'src/app/services/event.service';
 export class HistorialComponent {
 
   isLoading: boolean = false;
+
+  documentos: DocumentoResumenInterface[] =
+    [
+      {
+        id: 10209,
+        fecha_hora: "05/10/2023 07:19",
+        saldo: 50.00,
+        cargo: 10.00,
+        descuento: 5.00,
+        total: 55.00
+      },
+      {
+        id: 10210,
+        fecha_hora: "06/10/2023 15:00",
+        saldo: 60.00,
+        cargo: 10.00,
+        descuento: 0.00,
+        total: 70.00
+      },
+      {
+        id: 10211,
+        fecha_hora: "07/10/2023 08:10",
+        saldo: 60.00,
+        cargo: 10.00,
+        descuento: 0.00,
+        total: 70.00
+      }
+    ]
+
   constructor(
     private _eventService: EventService,
   ) {
---
+++ src/app/displays/prc_documento_3/interfaces/documento-resumen.interface.ts
@@ -0,0 +1,8 @@
+export interface DocumentoResumenInterface {
+    id: number,
+    fecha_hora: string,
+    saldo: number,
+    cargo: number,
+    descuento: number,
+    total: number
+}
