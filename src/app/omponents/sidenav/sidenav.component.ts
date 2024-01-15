c74efb02c224ac2f09d68e7e6e4f7eac20c77db0
Catu <karlacatusurec@gmail.com>
2024-01-15 09:30:42 -0600

    sidenav para todo

--- src/app/omponents/sidenav/sidenav.component.ts
+++ src/app/omponents/sidenav/sidenav.component.ts
@@ -8,14 +8,14 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class SidenavComponent {
   //Abrir/Cerrar SideNav
-  @ViewChild('sidenav')
-  sidenav!: MatSidenav;
+  // @ViewChild('sidenav')
+  // sidenav!: MatSidenav;
   @ViewChild('sidenavend')
   sidenavend!: MatSidenav;
 
   //Abrir cerrar Sidenav
   close(reason: string) {
-    this.sidenav.close();
+    // this.sidenav.close();
     this.sidenavend.close();
   }
 }
