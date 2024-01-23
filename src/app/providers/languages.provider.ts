0ae1861d67befb919992f77e9cafa339b4129cd6
Catu <karlacatusurec@gmail.com>
2024-01-23 10:10:25 -0600

    nuevos idiomas con sus debidas traducciones en idioma Frances y Aleman

--- src/app/providers/languages.provider.ts
+++ src/app/providers/languages.provider.ts
@@ -2,20 +2,30 @@ import { LanguageInterface } from "../interfaces/language.interface";
 
 //todos los lenguajes
 export const languagesProvider: LanguageInterface[] = [
+    //español
     {
         names: [
             {
-                lrCode: "es-GT",
-                name: "Español (Guatemala)"
+                lrCode: "es-GT", //el idioma donde se muestra
+                name: "Español (Guatemala)" //como se muestra
             },
             {
                 lrCode: "en-US",
                 name: "Spanish (Guatemala)"
+            },
+            {
+                lrCode: "fr-FR",
+                name: "Espagnol (Guatemala)"
+            },
+            {
+                lrCode: "de-DE",
+                name: "Spanisch (Guatemala)"
             }
         ],
         lang: "es",
         reg: "GT"
     },
+    //ingles
     {
         names: [
             {
@@ -25,12 +35,65 @@ export const languagesProvider: LanguageInterface[] = [
             {
                 lrCode: "en-US",
                 name: "English (United States)"
+            },
+            {
+                lrCode: "fr-FR",
+                name: "Anglais (United States)"
+            },
+            {
+                lrCode: "de-DE",
+                name: "Englisch (Vereinigte Staaten)"
             }
         ],
         lang: "en",
         reg: "US"
     },
+    //frances
+    {
+        names: [
+            {
+                lrCode: "es-GT",
+                name: "Francés (Francia)"
+            },
+            {
+                lrCode: "en-US",
+                name: "French (France)"
+            },
+            {
+                lrCode: "fr-FR",
+                name: "Français (France)"
+            },
+            {
+                lrCode: "de-DE",
+                name: "Französisch (Frankreich)"
+            }
+        ],
+        lang: "fr",
+        reg: "FR"
+    },
+    {
+        names: [
+            {
+                lrCode: "es-GT",
+                name: "Alemán (Alemania)"
+            },
+            {
+                lrCode: "en-US",
+                name: "German (Germany)"
+            },
+            {
+                lrCode: "fr-FR",
+                name: "Allemand (Allemagne)"
+            },
+            {
+                lrCode: "de-DE",
+                name: "Deutsch (Deutschland)"
+            }
+        ],
+        lang: "de",
+        reg: "DE"
+    }
 ]
 
 //Lenguaje por defecto
-export const indexDefaultLang : number = 0;
+export const indexDefaultLang: number = 0;
