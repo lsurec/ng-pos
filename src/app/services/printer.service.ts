cde55f3db9cccf82a47a3527a75ee3da6a901a6e
Robert Morales <lsurec@demosoft.com.gt>
2024-01-24 16:52:45 -0600

    add ticket

--- src/app/services/printer.service.ts
+++ src/app/services/printer.service.ts
@@ -12,6 +12,68 @@ export class PrinterService {
     constructor(private _http: HttpClient) {
     }
 
+    private _postPrint(
+        file: File,
+        printer: string,
+        copies: number,
+    ) {
+        const formData = new FormData();
+        formData.append('files', file);
+        //configurar headers
+        let headers = new HttpHeaders(
+            {
+                "printerName": printer,
+                "copies": copies,
+            }
+        )
+        //consumo de api
+        return this._http.post(`${this._urlBase}Printer`, formData, { headers: headers, observe: 'response' });
+    }
+
+    postPrint(
+        file: File,
+        printer: string,
+        copies: number,): Promise<ResApiInterface> {
+        return new Promise((resolve, reject) => {
+            this._postPrint(
+                file,
+                printer,
+                copies,
+            ).subscribe(
+                //si esta correcto
+                res => {
+
+                    let resApi: ResApiInterface = {
+                        status: true,
+                        response: res.body,
+                    }
+                    resolve(resApi);
+                },
+                //si algo sale mal
+                err => {
+                    try {
+                        let message = err.message;
+
+                        let resApi: ResApiInterface = {
+                            status: false,
+                            response: message,
+                            url: err.url,
+                        }
+                        resolve(resApi);
+
+                    } catch (ex) {
+                        let resApi: ResApiInterface = {
+                            status: false,
+                            response: err,
+                            url: err.url,
+                        }
+                        resolve(resApi);
+                    }
+                }
+            )
+        })
+    }
+
     //funcion que va a realizar el consumo privado para obtener las empresas
     private _getPrinters() {
 
