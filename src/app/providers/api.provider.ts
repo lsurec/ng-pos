533f8ea5f8e33068ea47c43419ec882a31534023
Catu <karlacatusurec@gmail.com>
2024-01-25 15:32:03 -0600

    url de api providers

--- src/app/providers/api.provider.ts
+++ src/app/providers/api.provider.ts
@@ -2,7 +2,8 @@ declare const settings: any
 
 export const urlApi = {
     apiServer: {
-        urlPrint: "https://localhost:7067/api/",
+        // urlPrint: "https://localhost:7067/api/",
+        urlPrint: "https://localhost:5001/api/",
         // urlBase: settings.urlBase,
         // urlBase: "http://localhost:9097/api/",
     }
