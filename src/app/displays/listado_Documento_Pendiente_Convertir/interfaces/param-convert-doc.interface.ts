fac11f71e0b00449a8b1b7ca8d7924a10792c091
Robert Morales <lsurec@demosoft.com.gt>
2024-02-20 12:01:10 -0600

    add servces

---
+++ src/app/displays/listado_Documento_Pendiente_Convertir/interfaces/param-convert-doc.interface.ts
@@ -0,0 +1,13 @@
+export interface ParamConvertDocInterface {
+    pUserName:           string;
+    pO_Documento:        number;
+    pO_Tipo_Documento:   number;
+    pO_Serie_Documento:  string;
+    pO_Empresa:          number;
+    pO_Estacion_Trabajo: number;
+    pO_Fecha_Reg:        number;
+    pD_Tipo_Documento:   number;
+    pD_Serie_Documento:  string;
+    pD_Empresa:          number;
+    pD_Estacion_Trabajo: number;
+}
