57428eebafc5c35ad21d3caa1197bf689a41c091
Karla Catu <kcatu@demosoft.com.gt>
2024-02-21 15:17:13 -0600

    navegacion a conversion documeto o navegacion a documento destino

--- src/app/displays/listado_Documento_Pendiente_Convertir/services/global-convert.service.ts
+++ src/app/displays/listado_Documento_Pendiente_Convertir/services/global-convert.service.ts
@@ -19,6 +19,8 @@ export class GlobalConvertService {
     docSelect?: TypesDocConvertInterface;
     docs: TypesDocConvertInterface[] = [];
     screen: string = "";
+
+    docDestino?: number;
     //vacio regresar a TypesDocsComponent
     //si tiene algo regresar a hoe
 
