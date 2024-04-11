76e4ab345a0a1beaf791aa193ca8ae8e668a0bc6
Robert Morales <lsurec@demosoft.com.gt>
2024-04-11 16:48:08 -0600

    update model for deyails origin docs

--- src/app/displays/listado_Documento_Pendiente_Convertir/interfaces/detail-origin-doc.interface.ts
+++ src/app/displays/listado_Documento_Pendiente_Convertir/interfaces/detail-origin-doc.interface.ts
@@ -1,17 +1,20 @@
 export interface DetailOriginDocInterface {
-    consecutivo_Interno: number;
-    disponible:          number;
-    clase:               string;
-    marca:               null | string;
-    id:                  string;
-    produc:              string;
-    bodega:              string;
-    cantidad:            number;
-    producto:            string;
-    unidad_Medida:       number;
-    fDes_Unidad_Medida:  string;
-    tipo_Precio:         number;
+    consecutivo_Interno:  number;
+    disponible:           number;
+    clase:                string;
+    marca:                null | string;
+    id:                   string;
+    producto_Descripcion: string;
+    bodega_Descripcion:   string;
+    cantidad:             number;
+    producto:             number;
+    unidad_Medida:        number;
+    fDes_Unidad_Medida:   string;
+    tipo_Precio:          number;
+    bodega:               number;
+    monto:                number;
 }
+
 
 
 export interface DetailOriginDocInterInterface {
