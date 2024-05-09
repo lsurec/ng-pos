c55111b547dfaffeae916e74734a6e5db7519746
Karla Catu <kcatu@demosoft.com.gt>
2024-05-09 13:02:18 -0600

    pruebas para el desplazamiento de las imagenes de un producto

--- src/app/displays/prc_documento_3/components/imagen/imagen.component.ts
+++ src/app/displays/prc_documento_3/components/imagen/imagen.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { ProductoInterface } from '../../interfaces/producto.interface';
+import { ImagenProductoInterface, ProductoInterface } from '../../interfaces/producto.interface';
 
 @Component({
   selector: 'app-imagen',
@@ -10,15 +10,18 @@ import { ProductoInterface } from '../../interfaces/producto.interface';
 export class ImagenComponent {
   isLoading: boolean = false; //pantalla de carga
   producto?: ProductoInterface;
-
+  imagenes: string[];
+  indexImagen = 1;
   constructor(
     //Instancias de los servicios qeu se van  a usar
     public dialogRef: MatDialogRef<ImagenComponent>,
-    @Inject(MAT_DIALOG_DATA) public imagen: ProductoInterface,
-
+    @Inject(MAT_DIALOG_DATA) public imagenesProducto: ImagenProductoInterface,
   ) {
 
-    this.producto = imagen;
+    this.imagenes = imagenesProducto.imagenesUrl;
+    this.urlVisible = this.imagenes[this.indexImagen];
+    console.log(this.urlVisible);
+    
   }
 
 
@@ -27,4 +30,25 @@ export class ImagenComponent {
     this.dialogRef.close();
   }
 
+  urlVisible: string = "";
+
+  siguiente() {
+
+    this.indexImagen = this.indexImagen + 1;
+
+    this.urlVisible = this.imagenes[this.indexImagen];
+    console.log(this.indexImagen);
+  }
+
+
+  anterior() {
+    this.indexImagen = this.indexImagen - 1;
+
+    this.urlVisible = this.imagenes[this.indexImagen];
+
+
+    console.log(this.indexImagen);
+    console.log(this.imagenes[this.indexImagen]);
+
+  }
 }
