3177fb48489fa05a1dffd7cb972934b6cb68641c
Karla Catu <kcatu@demosoft.com.gt>
2024-05-10 15:16:16 -0600

    controlar el tamaño de la fuente en todo el proyecto a excepcion de textos que deben tener un tamaño fijo. se logro por medio de las preferencias

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -8,8 +8,8 @@ export class ThemeService {
     //tema configurado: falso = claro; true = oscuro
     isDarkTheme: boolean = false;
     // Tamaño de fuente global
-    fontSize: string = "20px";
 
+    globalFontSize: string = "14px";
 
     //cambiar tema
     toggleTheme(): void {
@@ -25,13 +25,6 @@ export class ThemeService {
         } else {
             body.classList.remove('dark-theme');
         }
-        // Aplicar el tamaño de fuente global
-        body.style.fontSize = this.fontSize;
     }
 
-    size() {
-        const body = document.body; //tiene toda la aplicacion 
-        body.style.fontSize = this.fontSize;
-
-    }
 }
