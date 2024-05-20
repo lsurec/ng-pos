21696e532dd87c2b001ec318614db7ace6df7bd8
Karla Catu <kcatu@demosoft.com.gt>
2024-05-20 16:29:51 -0600

    agregar arrchivo provider de las horas

---
+++ src/app/providers/horas.provider.ts
@@ -0,0 +1,128 @@
+import { HoraInterface } from "../displays/prcTarea_1/interfaces/hora.interface";
+
+//horas del dia
+export const horas: HoraInterface[] = [
+    {
+        "hora24": 0,
+        "hora12": "12:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 1,
+        "hora12": "1:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 2,
+        "hora12": "2:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 3,
+        "hora12": "3:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 4,
+        "hora12": "4:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 5,
+        "hora12": "5:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 6,
+        "hora12": "6:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 7,
+        "hora12": "7:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 8,
+        "hora12": "8:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 9,
+        "hora12": "9:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 10,
+        "hora12": "10:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 11,
+        "hora12": "11:00 am",
+        "visible": true
+    },
+    {
+        "hora24": 12,
+        "hora12": "12:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 13,
+        "hora12": "1:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 14,
+        "hora12": "2:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 15,
+        "hora12": "3:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 16,
+        "hora12": "4:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 17,
+        "hora12": "5:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 18,
+        "hora12": "6:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 19,
+        "hora12": "7:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 20,
+        "hora12": "8:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 21,
+        "hora12": "9:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 22,
+        "hora12": "10:00 pm",
+        "visible": true
+    },
+    {
+        "hora24": 23,
+        "hora12": "11:00 pm",
+        "visible": true
+    }
+];
+
+export const indexHoraInicioDefault: number = 7; // 7:00am
+export const indexHoraFinDefault: number = 17; // 5:00 pm
