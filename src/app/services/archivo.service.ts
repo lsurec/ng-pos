42292fe2ba38fb6511529689f1f8f6fe83f19abf
Karla Catu <kcatu@demosoft.com.gt>
2024-06-25 11:40:15 -0600

    nuevo servicio para adnuntar los archivos a los comentarios

--- src/app/services/archivo.service.ts
+++ src/app/services/archivo.service.ts
@@ -111,4 +111,55 @@ export class CargarArchivosService {
             )
         })
     }
+
+
+    //Nuevo servicio
+    //TODO: utilizar cuando las Apis estén actualizadas
+    private _postFilesCommentN(files: File[], tarea: number, tareaComentario: number, urlCarpeta: string) {
+        const formData = new FormData();
+        for (const file of files) {
+            formData.append('files', file);
+        }
+        //configurar headers
+        let headers = new HttpHeaders(
+            {
+                "Authorization": "bearer " + this._token,
+                "user": this._user,
+                "tarea": tarea.toString(),
+                "tareaComentario": tareaComentario.toString(),
+                "urlCarpeta": urlCarpeta
+            }
+        )
+        //consumo de api
+        return this._http.post(`${this._urlBase}Tareas/objetos/comentario`, formData, { headers: headers });
+    }
+
+    postFilesCommentN(files: File[], tarea: number, tareaComentario: number, urlCarpeta: string): Promise<ResApiInterface> {
+        return new Promise((resolve, reject) => {
+            this._postFilesCommentN(files, tarea, tareaComentario, urlCarpeta).subscribe(
+                //si esta correcto
+                res => {
+                    let response: ResponseInterface = <ResponseInterface>res;
+
+                    let resApi: ResApiInterface = {
+                        status: true,
+                        response: response.data,
+                        storeProcedure: response.storeProcedure
+                    }
+                    resolve(resApi);
+                },
+                //si algo sale mal
+                err => {
+                    let response: ResponseInterface = <ResponseInterface>err;
+
+                    let resApi: ResApiInterface = {
+                        status: false,
+                        response: response.data,
+                        storeProcedure: response.storeProcedure
+                    }
+                    resolve(resApi);
+                }
+            )
+        })
+    }
 }
