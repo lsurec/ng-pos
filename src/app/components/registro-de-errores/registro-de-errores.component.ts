4e6a2e3c75cf5f5914ade4ec3c980c40a3fe6c95
Robert Morales <lsurec@demosoft.com.gt>
2024-06-26 10:53:04 -0600

    "Added imports, implemented OnInit, and created methods for loading data in RegistroDeErroresComponent"

--- src/app/components/registro-de-errores/registro-de-errores.component.ts
+++ src/app/components/registro-de-errores/registro-de-errores.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResApiInterface } from 'src/app/interfaces/res-api.interface';
+import { ErrorService } from 'src/app/services/error.service';
+import { PreferencesService } from 'src/app/services/preferences.service';
 
 @Component({
   selector: 'app-registro-de-errores',
   templateUrl: './registro-de-errores.component.html',
-  styleUrls: ['./registro-de-errores.component.scss']
+  styleUrls: ['./registro-de-errores.component.scss'],
+  providers: [
+    ErrorService,
+  ]
 })
-export class RegistroDeErroresComponent {
+export class RegistroDeErroresComponent implements OnInit {
+
+  isLoading: boolean = false;
+  token: string = PreferencesService.token;
+
+
+  constructor(
+    private _errorService: ErrorService,
+
+  ) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.laodData();
+  }
+
+  async laodData() {
+    this.isLoading = true;
+
+
+    let resApi: ResApiInterface = await this._errorService.getError(this.token);
+
+    console.log(resApi);
+
+
+
+    this.isLoading = false;
+  }
+
+
+
 
 }
