2c6e5205dacd6e58f4e5ad3d51e219874ea24b60
Karla Catu <kcatu@demosoft.com.gt>
2024-07-05 10:18:38 -0600

    nuevas variables globales para los colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { PreferencesService } from './services/preferences.service';
 import { LanguageInterface } from './interfaces/language.interface';
 import { languagesProvider, indexDefaultLang } from './providers/languages.provider';
 import { ThemeService } from './services/theme.service';
+import { ColoresInterface } from './interfaces/filtro.interface';
 
 @Component({
   selector: 'app-root',
@@ -75,6 +76,73 @@ export class AppComponent implements OnInit {
     document.documentElement.style.setProperty('--global-color-fondo', this._themeService.fondo);
     document.documentElement.style.setProperty('--global-color', this._themeService.color);
 
+    //Nuevos estilos para tema claro
+    document.documentElement.style.setProperty('--primario-oscuro', this.oscuro.primario);
+    document.documentElement.style.setProperty('--fondo-oscuro', this.oscuro.fondo);
+    document.documentElement.style.setProperty('--bloques-oscuro', this.oscuro.bloques);
+    document.documentElement.style.setProperty('--seleccionado-oscuro', this.oscuro.seleccionado);
+    document.documentElement.style.setProperty('--descuento-oscuro', this.oscuro.descuento);
+    document.documentElement.style.setProperty('--cargo-oscuro', this.oscuro.cargo);
+    document.documentElement.style.setProperty('--saldo-oscuro', this.oscuro.saldo);
+    document.documentElement.style.setProperty('--scroll-oscuro', this.oscuro.scroll);
+    document.documentElement.style.setProperty('--bordes-oscuro', this.oscuro.bordes);
+    document.documentElement.style.setProperty('--navegacion-oscuro', this.oscuro.navegacion);
+    document.documentElement.style.setProperty('--sombra-oscuro', this.oscuro.sombra);
+    document.documentElement.style.setProperty('--pestana-oscuro', this.oscuro.pestana);
+    document.documentElement.style.setProperty('--iconos-oscuro', this.oscuro.iconos);
+    document.documentElement.style.setProperty('--boton-ico-oscuro', this.oscuro.botonIcono);
+
+    //Tema Claro
+    document.documentElement.style.setProperty('--primario-claro', this.claro.primario);
+    document.documentElement.style.setProperty('--fondo-claro', this.claro.fondo);
+    document.documentElement.style.setProperty('--bloques-claro', this.claro.bloques);
+    document.documentElement.style.setProperty('--seleccionado-claro', this.claro.seleccionado);
+    document.documentElement.style.setProperty('--descuento-claro', this.claro.descuento);
+    document.documentElement.style.setProperty('--cargo-claro', this.claro.cargo);
+    document.documentElement.style.setProperty('--saldo-claro', this.claro.saldo);
+    document.documentElement.style.setProperty('--scroll-claro', this.claro.scroll);
+    document.documentElement.style.setProperty('--bordes-claro', this.claro.bordes);
+    document.documentElement.style.setProperty('--navegacion-claro', this.claro.navegacion);
+    document.documentElement.style.setProperty('--sombra-claro', this.claro.sombra);
+    document.documentElement.style.setProperty('--pestana-claro', this.claro.pestana);
+    document.documentElement.style.setProperty('--iconos-claro', this.claro.iconos);
+    document.documentElement.style.setProperty('--boton-ico-claro', this.claro.botonIcono);
   }
+
+  claro: ColoresInterface = {
+    primario: "#134895",
+    fondo: "#FEF5E7",
+    bloques: "rgba(0, 0, 0, 0.12)",
+    seleccionado: "#777",
+    descuento: "#ce1414",
+    cargo: "#25a704",
+    saldo: "#134895",
+    scroll: "#134895",
+    bordes: "rgba(0, 0, 0, 0.12)",
+    navegacion: "#f0f0f0",
+    sombra: "0px 1px 0px 1px rgba(0, 0, 0, 0.1)",
+    pestana: "#134895",
+    iconos: "#777",
+    botonIcono: "#fffff",
+  }
+
+
+  oscuro: ColoresInterface = {
+    primario: "#134895",
+    fondo: "#302f2f",
+    bloques: "rgba(127, 121, 121, 0.31)",
+    seleccionado: "#5f5f5f",
+    descuento: "#ff3a3a",
+    cargo: "#44ff14",
+    saldo: "#4d91f5",
+    scroll: "#635f5f",
+    bordes: "#8a8484",
+    navegacion: "#252323",
+    sombra: "0px 1px 0px 1px rgba(164, 164, 164, 0.29)",
+    pestana: "#df9722",
+    iconos: "#c4c4c4",
+    botonIcono: "#c4c4c4"
+  };
+
 
 }
--- src/app/interfaces/filtro.interface.ts
+++ src/app/interfaces/filtro.interface.ts
@@ -1,10 +1,27 @@
 export interface FiltroInterface {
-    id:     number,
+    id: number,
     nombre: string,
 }
 
 export interface ColorInterface {
-    id:     number,
-    valor:  string,
-    nombre:  string,
+    id: number,
+    valor: string,
+    nombre: string,
+}
+
+export interface ColoresInterface {
+    primario: string;
+    fondo: string;
+    bloques: string;
+    seleccionado: string;
+    descuento: string;
+    cargo: string;
+    saldo: string;
+    scroll: string;
+    bordes: string;
+    navegacion: string;
+    sombra: string;
+    pestana: string;
+    iconos: string;
+    botonIcono: string;
 }
