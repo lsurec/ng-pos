de715b602eee8ecd6edcef2f088aac0ba2489bc8
Karla Catu <kcatu@demosoft.com.gt>
2024-07-05 14:36:11 -0600

    pantalla de url api lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,6 +80,8 @@ export class AppComponent implements OnInit {
     document.documentElement.style.setProperty('--seleccionado2', "#5d6d6c");
     document.documentElement.style.setProperty('--blanco', "#fff");
     document.documentElement.style.setProperty('--texto-boton', "#cdc7c7");
+    document.documentElement.style.setProperty('--gris', "#777");
+
 
 
     //Nuevos estilos para tema claro
