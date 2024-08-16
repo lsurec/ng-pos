38f30db5bdcfa567da919bd7713790d72fae73ae
Karla Catu <kcatu@demosoft.com.gt>
2024-08-16 09:08:35 -0600

    correccion en detalle tipados de variables y validaxiones (correccion en traduccion)

--- src/app/displays/prc_documento_3/components/detalle/detalle.component.ts
+++ src/app/displays/prc_documento_3/components/detalle/detalle.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FiltroInterface } from '../../interfaces/filtro.interface';
 import { ImagenProductoInterface, ProductoInterface } from '../../interfaces/producto.interface';
 import { MatDialog } from '@angular/material/dialog';
-import { ProductosEncontradosComponent } from '../productos-encontrados/productos-encontrados.component';
 import { NotificationsService } from 'src/app/services/notifications.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ProductService } from '../../services/product.service';
@@ -23,6 +22,7 @@ import { DataUserService } from '../../services/data-user.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
 import { ValidateProductInterface } from 'src/app/displays/listado_Documento_Pendiente_Convertir/interfaces/validate-product.interface';
 import { PrecioDiaInterface } from '../../interfaces/precio-dia.interface';
+import { TypeErrorInterface } from 'src/app/interfaces/type-error.interface';
 
 @Component({
   selector: 'app-detalle',
@@ -47,6 +47,7 @@ export class DetalleComponent implements AfterViewInit {
   estacion: number = PreferencesService.estacion.estacion_Trabajo; //estacion de la sesion
   documento: number = this.facturaService.tipoDocumento!; //Tipo docuemtno seleccioando (display)
   tipoDesCar: number = 1; //tipo de cargo o descuento (monto o porcentaje)
+  tipoCambio: number = PreferencesService.tipoCambio;
 
 
 
@@ -97,6 +98,7 @@ export class DetalleComponent implements AfterViewInit {
   //editar transacciines que ay fueron agregadas
   async editTra(indexTra: number) {
 
+    this.facturaService.isLoading = true;
 
     //Limpiar bodegas previas
     this.productoService.bodegas = [];
@@ -107,7 +109,7 @@ export class DetalleComponent implements AfterViewInit {
     this.productoService.cantidad = this.facturaService.traInternas[indexTra].cantidad.toString();
 
 
-    let productTra = this.facturaService.traInternas[indexTra].producto; //producto de la transaccion
+    let productTra: ProductoInterface = this.facturaService.traInternas[indexTra].producto; //producto de la transaccion
     let boddegaTra = this.facturaService.traInternas[indexTra].bodega; // bodega  de la transaccion
     let precioTra = this.facturaService.traInternas[indexTra].precio; //tipo precio de la transaccion
 
@@ -343,7 +345,7 @@ export class DetalleComponent implements AfterViewInit {
 
     //abrir dialogo producto con lo datos cargados
 
-    let resDialogProd = await this._notificationsService.openDetalleporoduct(productTra);
+    let resDialogProd: TypeErrorInterface = await this._notificationsService.openDetalleporoduct(productTra);
 
     this.productoService.cantidad = "1";
 
@@ -659,6 +661,8 @@ export class DetalleComponent implements AfterViewInit {
 
       }
 
+      //TODO: evaluar precios vacios
+
       //si solo ahy precio seleccoanrlo por defectp
       if (this.productoService.precios.length == 1) {
 
@@ -679,8 +683,8 @@ export class DetalleComponent implements AfterViewInit {
             this.productoService.precioU = element.precioU;
             this.productoService.precioText = element.precioU.toString();
 
+            break;
           }
-          break;
 
         }
 
@@ -735,8 +739,6 @@ export class DetalleComponent implements AfterViewInit {
 
     //si no se abre el dialogo agregar ka transaccon directammente
 
-    //TODO:Hacer validaciones y agreagr transaccion
-
     if (!this.productoService.bodega!.posee_Componente) {
       let resDisponibiladProducto: ResApiInterface = await this._productService.getValidateProducts(
         this.user,
@@ -749,7 +751,7 @@ export class DetalleComponent implements AfterViewInit {
         product.unidad_Medida,
         product.producto,
         UtilitiesService.convertirTextoANumero(this.productoService.cantidad)!,
-        8, //TODO:Parametrizar
+        this.tipoCambio,
         this.productoService.precio!.moneda,
         this.productoService.precio!.id,
         this.token,
@@ -803,7 +805,7 @@ export class DetalleComponent implements AfterViewInit {
     }
 
 
-      //Calcular totral de la transaccion
+    //Calcular totral de la transaccion
     //SI no hau precio seleccionado no calcular
     if (!this.productoService.precio) {
       this.productoService.total = 0;
@@ -908,20 +910,20 @@ export class DetalleComponent implements AfterViewInit {
     // /7agregar transaccion
     this.facturaService.addTransaction(
       {
-        consecutivo: 0,
-        estadoTra: 1,
-        precioCantidad: this.facturaService.valueParametro(44) ? this.productoService.total : null,
-        precioDia: this.facturaService.valueParametro(44) ? precioDias : null,
-        isChecked: false,
         bodega: this.productoService.bodega,
-        producto: product,
-        precio: this.productoService.precio!,
         cantidad: UtilitiesService.convertirTextoANumero(this.productoService.cantidad)!,
         cantidadDias: this.facturaService.valueParametro(44) ? cantidadDias : 0,
-        total: this.facturaService.valueParametro(44) ? precioDias : this.productoService.total,
         cargo: 0,
+        consecutivo: 0,
         descuento: 0,
+        estadoTra: 1,
+        isChecked: false,
         operaciones: [],
+        precio: this.productoService.precio!,
+        precioCantidad: this.facturaService.valueParametro(44) ? this.productoService.total : null,
+        precioDia: this.facturaService.valueParametro(44) ? precioDias : null,
+        producto: product,
+        total: this.facturaService.valueParametro(44) ? precioDias : this.productoService.total,
       }
     );
 
