88185265d494d85ec5fe86979b78bb39e1d1518b
Robert Morales <lsurec@demosoft.com.gt>
2024-09-04 16:04:14 -0600

    Added two new text elements to the printer service configuration.

--- src/app/services/printer.service.ts
+++ src/app/services/printer.service.ts
@@ -1376,6 +1376,29 @@ export class PrinterService {
                                     ]
                                 }
                             ],
+                            [
+                                {
+                                    text: "TIPO CLIENTE:",
+                                    style: 'normalTextBold',
+                                },
+                                {
+                                    text: doc.cliente.tipo,
+                                    style: 'normalText',
+                                }
+                                ,
+                                {
+                                    text: [
+                                        {
+                                            text: "Tipo evento: ",
+                                            style: 'normalTextBold',
+                                        },
+                                        {
+                                            text: doc.documento.evento,
+                                            style: 'normalText',
+                                        }
+                                    ]
+                                }
+                            ],
                         ]
                     }
                 },
