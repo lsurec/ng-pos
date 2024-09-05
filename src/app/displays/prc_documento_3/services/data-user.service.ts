01b976989806c5936a37abc73e2fee2b80717792
Karla Catu <kcatu@demosoft.com.gt>
2024-09-05 10:09:33 -0600

    cambio de version a 1.0.7

--- src/app/displays/prc_documento_3/services/data-user.service.ts
+++ src/app/displays/prc_documento_3/services/data-user.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class DataUserService {
 
 
-  version:string = "1.0.6";
+  version:string = "1.0.7";
 
   //Nombre del display al que se está navegando
   nameDisplay: string = "";
