4bfbc60ef4b015a58553905d87e525f4543e725f
Robert Morales <lsurec@demosoft.com.gt>
2024-09-30 12:20:42 -0600

    "Refactor ApiService: updated apiUse method to handle errors and return response in ResApiInterface format"

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,24 +10,31 @@ import { firstValueFrom, Observable } from 'rxjs';
 export class ApiService {
 
 
-    // función asíncrona para obtener las empresas con menos código repetitivo
+    // función asíncrona para uso de apis, manejo de errores
     static async apiUse(api: () => Observable<any>): Promise<ResApiInterface> {
         try {
+            //Uso del api
             const res = await firstValueFrom(api());
+
+            //Resultado del api
             const response: ResponseInterface = res.body as ResponseInterface;
 
+            //Retronarn respuesta correcta
             return {
                 status: true,
                 response: response.data,
                 storeProcedure: response.storeProcedure
             };
         } catch (err: any) {
+            //Si existen errores
             let resApi: ResApiInterface = {
                 status: false,
                 response: err?.error?.data || err.message || 'Unknown error',
                 storeProcedure: err?.error?.storeProcedure,
                 url: err.url
             };
+
+            //Retonra el error con la interfaz requerida
             return resApi;
         }
     }
