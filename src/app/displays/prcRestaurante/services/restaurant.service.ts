99f7ecefdf43fa13cd10a28777dacad9818ba3fa
Robert Morales <lsurec@demosoft.com.gt>
2024-10-01 08:06:08 -0600

    "Added two new methods to RestaurantService: getTables and getAccountPin, which make GET requests to API endpoints with custom headers."

--- src/app/displays/prcRestaurante/services/restaurant.service.ts
+++ src/app/displays/prcRestaurante/services/restaurant.service.ts
@@ -34,4 +34,48 @@ export class RestaurantService {
         return this._http.get(`${this._urlBase}Restaurant/locations`, { headers: headers, observe: 'response' });
     }
 
+
+    // función que va a realizar el consumo privado para obtener las empresas
+    getTables(
+        typeDoc: number,
+        enterprise: number,
+        station: number,
+        series: string,
+        elementAssigned: number,
+        user: string,
+        token: string,
+
+    ) {
+        const headers = new HttpHeaders({
+            "Authorization": "bearer " + token,
+            "typeDoc": typeDoc,
+            "enterprise": enterprise,
+            "station": station,
+            "series": series,
+            "elementAssigned": elementAssigned,
+            "user": user,
+            "token": token,
+        });
+
+        // consumo de api
+        return this._http.get(`${this._urlBase}Restaurant/tables`, { headers: headers, observe: 'response' });
+    }
+
+    getAccountPin(
+        token: string,
+        enterprice: number,
+        pin: string,
+
+    ) {
+        const headers = new HttpHeaders({
+            "Authorization": "bearer " + token,
+            "enterprise": enterprice,
+            "pin": pin,
+        });
+
+        // consumo de api
+        return this._http.get(`${this._urlBase}Restaurant/account/pin`, { headers: headers, observe: 'response' });
+    }
+
+
 }
