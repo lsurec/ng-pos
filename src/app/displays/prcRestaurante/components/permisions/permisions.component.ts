fbe2f9c1678472bf5b0760e7da0bcd4412b98d9e
Robert Morales <lsurec@demosoft.com.gt>
2024-10-15 10:13:42 -0600

    "Added imports, interfaces, and services to PermisionsComponent; implemented login functionality with API calls and error handling; created TipoAccionService with HTTP client and getTipoAccion method."

--- src/app/displays/prcRestaurante/components/permisions/permisions.component.ts
+++ src/app/displays/prcRestaurante/components/permisions/permisions.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
+import { LoginInterface } from 'src/app/interfaces/login.interface';
+import { ResApiInterface } from 'src/app/interfaces/res-api.interface';
+import { UserInterface } from 'src/app/interfaces/user.interface';
+import { ApiService } from 'src/app/services/api.service';
 import { NotificationsService } from 'src/app/services/notifications.service';
+import { GlobalRestaurantService } from '../../services/global-restaurant.service';
+import { LoginService } from 'src/app/services/login.service';
+import { TipoAccionService } from 'src/app/services/tipo-accion.service';
+import { PreferencesService } from 'src/app/services/preferences.service';
+import { ErrorInterface } from 'src/app/interfaces/error.interface';
 
 @Component({
   selector: 'app-permisions',
   templateUrl: './permisions.component.html',
-  styleUrls: ['./permisions.component.scss']
+  styleUrls: ['./permisions.component.scss'],
+  providers: [
+    LoginService,
+    TipoAccionService,
+  ]
 })
 export class PermisionsComponent {
 
@@ -18,6 +31,9 @@ export class PermisionsComponent {
     private _translate: TranslateService,
     public dialogRef: MatDialogRef<PermisionsComponent>,
     private _notificationService: NotificationsService,
+    private _restauranteService: GlobalRestaurantService,
+    private _loginService: LoginService,
+    private _tipoAccionService: TipoAccionService,
   ) {
   }
 
@@ -28,14 +44,107 @@ export class PermisionsComponent {
     this.dialogRef.close();
   }
 
-  ingresar() {
+  async ingresar() {
     //Sino hay usuario ni contrase;a mostrar notificacion de que debe completar
     if (!this.nombre || !this.clave) {
       this._notificationService.openSnackbar(this._translate.instant('pos.alertas.completar'));
       return
     }
 
+
+    //Interface de credenciales
+    let formValues: UserInterface = {
+      pass: this.nombre,
+      user: this.clave
+    };
+
+    //antes de ejecutarse
+    this._restauranteService.isLoading = true;
+
+
+    const apiLogin = () => this._loginService.postLogin(formValues);
+
+    let res: ResApiInterface = await ApiService.apiUse(apiLogin);; //consumo del api
+
+
+    ///Si el servico se ejecuta mal mostar menaje
+    if (!res.status) {
+      //TODO:Pantalla de error
+      this._restauranteService.isLoading = false;
+
+      this.showError(res);
+      return;
+    };
+
+    //Si el servicio se ejucuto ben
+    //Obtener la respuesta del api login
+    let resLogin: LoginInterface = res.response;
+    //si algo esta incorrecto mostrar mensaje
+
+    if (!resLogin.success) {
+      this._restauranteService.isLoading = false;
+      this._notificationService.openSnackbar(this._translate.instant('pos.alertas.incorrecto'));
+      this.dialogRef.close();
+      return;
+    };
+
+    //validar tipo accion
+    const apiTipoAccion = () => this._tipoAccionService.getTipoAccion(
+      45, //45 transaccion, 32 mesa
+      resLogin.user,
+      resLogin.message,
+    );
+
+    let resTipoAccion = await ApiService.apiUse(apiTipoAccion);
+
+
+    if (!resTipoAccion.status) {
+
+      //TODO:Pantalla de error
+      this._restauranteService.isLoading = false;
+
+      this.showError(resTipoAccion);
+      this.dialogRef.close();
+      return;
+    };
+
     this.dialogRef.close(true);
+  }
+
+
+  async showError(res: ResApiInterface) {
+
+    //Diaogo de confirmacion
+    let verificador = await this._notificationService.openDialogActions(
+      {
+        title: this._translate.instant('pos.alertas.salioMal'),
+        description: this._translate.instant('pos.alertas.error'),
+        verdadero: this._translate.instant('pos.botones.informe'),
+        falso: this._translate.instant('pos.botones.aceptar'),
+      }
+    );
+
+    //Cancelar
+    if (!verificador) return;
+
+    let dateNow: Date = new Date(); //fecha del error
+
+    //Crear error
+    let error: ErrorInterface = {
+      date: dateNow,
+      description: res.response,
+      storeProcedure: res.storeProcedure,
+      url: res.url,
+    }
+
+    //Guardar error
+    PreferencesService.error = error;
+
+    //TODO:mostrar pantalla de error
+
+    this._restauranteService.verError = true;
+
+    return;
   }
 
 }
--- src/app/services/tipo-accion.service.ts
+++ src/app/services/tipo-accion.service.ts
@@ -1,7 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PreferencesService } from './preferences.service';
 
 @Injectable()
 export class TipoAccionService{
   
+    private _urlBase: string = PreferencesService.baseUrl;
+
+    //inicializar http
+    constructor(private _http: HttpClient) {
+    }
     
+    getTipoAccion(
+         tipoAccion:number,
+     user: string,
+     token: string,
+    ) {
+        //confgurar headers
+
+        let headers = new HttpHeaders(
+            {
+                "Authorization": "bearer " + token,
+            }
+        )
+        //consumo de api
+        return this._http.get(`${this._urlBase}Usuarios/accion/${user}/${tipoAccion}`, { headers: headers, observe: 'response' });
+    }
 }
