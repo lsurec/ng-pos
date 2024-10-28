acb2b6e4aa90d849cb1912c5e0767b415ec273ce
Karla Catu <kcatu@demosoft.com.gt>
2024-10-28 16:31:36 -0600

    diseño detalle de apis

--- src/app/displays/apis_fel/components/api-detalle/api-detalle.component.ts
+++ src/app/displays/apis_fel/components/api-detalle/api-detalle.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { CertificadorService } from '../../services/certificador.service';
+import { TipoMetodoInterface } from '../../interfaces/tipo_metodo.interface';
+import { catalogoTipoMetodo } from '../../provider_temp/tipo_metodo';
+import { TipoDatoInterface } from '../../interfaces/tipo_dato.interface';
+import { catalogoTipoDato } from '../../provider_temp/tipo_dato';
+import { catalogoTipoRespuesta } from '../../provider_temp/tipo_respuesta';
+import { TipoRespuestaInterface } from '../../interfaces/tipo_respuesta.interface';
+import { catalogoTipoServicio } from '../../provider_temp/tipo_servicio';
+import { TipoServicioInterface } from '../../interfaces/tipo_servicio.interface';
 
 @Component({
   selector: 'app-api-detalle',
@@ -8,6 +16,10 @@ import { CertificadorService } from '../../services/certificador.service';
 })
 export class ApiDetalleComponent {
 
+  catalogoTipoMetodo: TipoMetodoInterface[] = catalogoTipoMetodo;
+  catalogoTipoDato: TipoDatoInterface[] = catalogoTipoDato;
+  catalogoTipoRespuesta: TipoRespuestaInterface[] = catalogoTipoRespuesta;
+  catalogoTipoServicio: TipoServicioInterface[] = catalogoTipoServicio;
 
   constructor(
     public mantenimiento: CertificadorService,
