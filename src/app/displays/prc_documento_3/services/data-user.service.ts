85bb6a23d39fc1c294a891f312f9f1a8f91ca632
Robert Morales <lsurec@demosoft.com.gt>
2024-10-30 14:33:41 -0600

    update data-user.service.ts

--- src/app/displays/prc_documento_3/services/data-user.service.ts
+++ src/app/displays/prc_documento_3/services/data-user.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class DataUserService {
 
 
-  version: string = "1.2.0";
+  version: string = "1.2.1";
 
   //Nombre del display al que se está navegando
   nameDisplay: string = "";
