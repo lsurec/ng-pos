ab75cf899cde568c9b066c6ab88147b49216a8e8
Robert Morales <lsurec@demosoft.com.gt>
2024-11-19 11:49:06 -0600

    "Updated version number in DataUserService from 1.2.3 to 1.2.4."

--- src/app/displays/prc_documento_3/services/data-user.service.ts
+++ src/app/displays/prc_documento_3/services/data-user.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class DataUserService {
 
 
-  version: string = "1.2.3";
+  version: string = "1.2.4";
 
   //Nombre del display al que se está navegando
   nameDisplay: string = "";
